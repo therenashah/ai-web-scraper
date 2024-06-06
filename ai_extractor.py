6b4e24e8d0111fff13caa7017f5b95e76b18cc43
Rena Shah <113020538+therenashah@users.noreply.github.com>
2024-06-06 17:47:49 +0530

    Update ai_extractor.py

--- ai_extractor.py
+++ ai_extractor.py
@@ -1,11 +1,9 @@
 import os
 
-from dotenv import load_dotenv
 from langchain.chains import (create_extraction_chain,
                               create_extraction_chain_pydantic)
 from langchain.chat_models import ChatOpenAI
 
-load_dotenv()
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
